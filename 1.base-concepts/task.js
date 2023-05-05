7e3131df0d867ada8d0a503dba6da9a71310b0bc
Юлия Минеева <zvetok.lolo2@gmail.com>
2023-05-05 13:53:10 +0500

    Исправленно

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = (Math.pow(b,2)-4*a*c)
-  if ( d < 0) {
-     arr = []
-  } else if (d == 0) {
+   if (d == 0) {
      arr = [(-b/(2*a))]
   } else if (d > 0) {
     arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)]
@@ -13,10 +11,10 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent/100/12;
-  let S = amount - contribution;
-  let n = countMonths
-  let Платёж = S * (P + (P / (((1 + P)**n) - 1)))
-  let Summa = Платёж * countMonths
-  return Number(Math.round(Summa*100)/100)
+  let monthlyPercentage = percent/100/12;
+  let loanBody = amount - contribution;
+  let count = countMonths
+  let pay = loanBody * (monthlyPercentage + (monthlyPercentage / (((1 + monthlyPercentage)**count) - 1)))
+  let summa = pay * countMonths
+  return Number(Math.round(summa*100)/100)
 }
