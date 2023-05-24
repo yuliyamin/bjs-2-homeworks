b3623a8bb75fa5187697255871bcc1d762fc8d4f
Юлия Минеева <zvetok.lolo2@gmail.com>
2023-05-24 18:34:45 +0500

    Готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = []
   
 }
+   //
+  // new Student(Boris);
+   //new Student(Stas)
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
+   this.subject = subjectName  
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+   if (this.hasOwnProperty('marks')) {
+    this.marks.push (...marks)
+   } 
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function () {  
+ if (!this.hasOwnProperty("marks") || this.marks.length === 0) {
+     return 0;
+   } else {
+     return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length; 
+   }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject
+  delete this.marks
+  this.excluded = reason
 }
